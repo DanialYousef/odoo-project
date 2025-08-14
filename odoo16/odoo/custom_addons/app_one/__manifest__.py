d1382448b7a0221d9864053893dd818ff1a14315
Your Name <danial60d@gmail.com>
2025-08-14 16:42:26 +0300

    add font style to the reports and add unit test to check the values of new record

--- odoo16/odoo/custom_addons/app_one/__manifest__.py
+++ odoo16/odoo/custom_addons/app_one/__manifest__.py
@@ -25,7 +25,9 @@
 
     ],
     'assets': {
-        'web.assets_backend' :['app_one\static\src\css\property.css']
+        'web.assets_backend' :['app_one\static\src\css\property.css'],
+        'web.report_assets_common': ['app_one\\static\\src\\css\\font.css']
+
     },
     'application': True,
 }
