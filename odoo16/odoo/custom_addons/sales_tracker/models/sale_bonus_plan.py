6cfad405872f6062469f6835735de169ae695483
DanialYousef <daneal.yosef.2000.123@gmail.com>
2025-10-09 13:40:44 +0300

    Editing Sales Tracker Module and Connect it with Accounting Module

--- odoo16/odoo/custom_addons/sales_tracker/models/sale_bonus_plan.py
+++ odoo16/odoo/custom_addons/sales_tracker/models/sale_bonus_plan.py
@@ -1,5 +1,6 @@
 
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 class BonusPlan(models.Model):
     _name = 'sale.bonus.plan'
@@ -14,16 +15,31 @@ class BonusPlan(models.Model):
         ('percentage','Percentage'),
         ('target','Target'),
     ], default='percentage')
-    bonus_value = fields.Float()
+    bonus_value = fields.Monetary(string='Bonus Value' ,currency_field="currency_id" )
     status = fields.Selection([
         ('draft','Draft'),
         ('active','Active'),
         ('closed','Closed')
     ], default='draft')
+    currency_id = fields.Many2one('res.currency', string='Currency' ,default=lambda self:self.env.company.currency_id.id)
+    expense_account_id =fields.Many2one('account.account' , string='Expense Account' , required=True )
+    payable_account_id = fields.Many2one('account.account' , string='Payable Account' , required=True )
+    journal_id = fields.Many2one('account.journal' , string='Journal' , required=True )
+    moved_id = fields.Many2one('account.move' , string='Journal Entry'  )
 
-    _sql_constraints = [
-        ('unique_name', 'unique("name")', 'This name is already in use')
-    ]
+    """ Create a restriction that prevents duplication of an existing name """
+    @api.constrains('name')
+    def _check_name(self):
+        for rec in self:
+            existing = self.search([
+                ('name', '=', rec.name),
+                ('id', '!=', rec.id)
+            ])
+            if existing:
+                raise ValidationError(f"The name '{rec.name}' already exists.")
+
+
+    """ Closed plan : Change status of plan to closed and Delete all record are connected with it  """
     def action_closed_plan(self):
         self.ensure_one()
         domain = [('plan_id.id','=',self.ids)]
@@ -33,6 +49,8 @@ class BonusPlan(models.Model):
         for rec in self:
             rec.status = 'closed'
 
+
+    """ Compute Bonus : Get All Sale Orders and Calculate Bonsu Depending on Total sales of each representative individually """
     def action_compute_bonus(self):
         print("inside action_compute_bonus")
         self.status = 'active'
@@ -53,14 +71,19 @@ class BonusPlan(models.Model):
             totals.setdefault(parent.id , 0)
             totals[parent.id] += o.amount_total
 
-        for pid , total in totals.items():
-            bonus= 0
+        high_total = 10000
+        min_total = 5000
+        for pid, total in totals.items():
+            bonus = 0
             if self.bonus_type == 'percentage':
                 bonus = total * (self.bonus_value / 100)
             elif self.bonus_type == 'target':
-                if total > 10000: bonus = self.bonus_value
-                elif total > 5000: bonus = self.bonus_value/2
-                else: bonus = 0
+                if total > high_total:
+                    bonus = self.bonus_value
+                elif total > min_total:
+                    bonus = self.bonus_value / 2
+                else:
+                    bonus = 0
 
             self.env['sale.bonus.record'].sudo().create({
                 'plan_id' : self.id,
@@ -68,4 +91,7 @@ class BonusPlan(models.Model):
                 'total_sales': total,
                 'calculated_bonus' : bonus,
                 'status' : 'draft',
+                'expense_account_id' : self.expense_account_id.id,
+                'payable_account_id' : self.payable_account_id.id,
+                'journal_id' : self.journal_id.id,
             })
--- odoo16/odoo/custom_addons/sales_tracker/models/sale_bonus_record.py
+++ odoo16/odoo/custom_addons/sales_tracker/models/sale_bonus_record.py
@@ -1,19 +1,65 @@
-from odoo import fields, models
+from odoo import fields, models , api
+from odoo.exceptions import ValidationError
+
 
 class BonusRecord(models.Model):
     _name = 'sale.bonus.record'
     _inherit = ['mail.thread' , 'mail.activity.mixin' ]
 
     plan_id = fields.Many2one('sale.bonus.plan', required=True)
-    partner_id = fields.Many2one('res.partner', required=True)
-    total_sales = fields.Float(string='Total Sales')
-    calculated_bonus = fields.Float(string='Calculated Bonus')
+    partner_id = fields.Many2one('res.partner')
+    total_sales = fields.Monetary(string='Total Sales' , currency_field="currency_id")
+    calculated_bonus = fields.Monetary(string='Calculated Bonus' , currency_field="currency_id")
+    currency_id = fields.Many2one('res.currency', string='Currency' , default=lambda self : self.env.company.currency_id.id)
     status = fields.Selection([
         ('draft','Draft'),
         ('approved','Approved')
     ], default='draft')
+    expense_account_id =fields.Many2one('account.account' , string='Expense Account' , required=True )
+    payable_account_id = fields.Many2one('account.account' , string='Payable Account' , required=True )
+    journal_id = fields.Many2one('account.journal' , string='Journal' , required=True )
+    moved_id = fields.Many2one('account.move' , string='Journal Entry' )
+    is_canceled = fields.Boolean(string='Is Canceled' , default=False)
 
+    """ Approved plan : Check tha result by manger ,
+     Update result and approved the plan to create financial restriction in Accounting module """
     def action_approved_state(self):
         print(self.plan_id.id)
         for rec in self:
-            rec.status = 'approved'
+            try:
+                move_vals = {
+                    'invoice_date' : fields.Date.context_today(self),
+                    'ref' : f'bonus - {rec.partner_id.name}',
+                    'journal_id' : rec.journal_id.id,
+                    'line_ids' : [
+                        (0,0,{
+                            'name' : f'bonus expense for {rec.partner_id.name}',
+                            'account_id' : rec.expense_account_id.id,
+                            'debit' : rec.calculated_bonus,
+                            'credit' : 0,
+                        }),
+                        (0,0,{
+                            'name': f'Bonus Payable to {rec.partner_id.name}',
+                            'account_id': rec.payable_account_id.id,
+                            'debit': 0.0,
+                            'credit': rec.calculated_bonus,
+                            'partner_id': rec.partner_id.id,
+                        })
+                    ]
+                }
+                move = self.env['account.move'].create(move_vals)
+                move.action_post()
+
+                rec.write({
+                    'status': 'approved',
+                    'moved_id': move.id,
+                })
+            except Exception as error:
+                raise ValidationError(str(error))
+
+    """ Change the value of column (is_canceled) of the open record """
+    def action_archive_state(self):
+        print('inside action archive state')
+        for rec in self:
+            rec.is_canceled = True
+
